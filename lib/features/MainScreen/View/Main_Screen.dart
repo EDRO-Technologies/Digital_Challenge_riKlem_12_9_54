e9840d6df5fd64193c38b2d4d6fd56f5ca45f8c1
hackaton <abramovviktor539@gmail.com>
2024-11-16 19:08:22 +0300

    upload2

--- lib/features/MainScreen/View/Main_Screen.dart
+++ lib/features/MainScreen/View/Main_Screen.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-import '../../Widgets/Custom_Navigation_Bar.dart';
-import '../Widgets/Custom_App_Bar.dart';
-import '../Widgets/Custom_List_Delegate.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -15,20 +10,6 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CustomScrollView(
-        controller: scrollController,
-        slivers: [
-          const CustomAppBar(),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                const CustomListDelegate(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
