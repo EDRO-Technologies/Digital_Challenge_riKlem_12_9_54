6c35a0cc8f06615fe79947667638f3b0c0d840aa
redlinegame <redlinegame10@gmail.com>
2024-11-17 09:19:29 +0300

    all

--- lib/features/LoginScreen/View/Login_Screen.dart
+++ lib/features/LoginScreen/View/Login_Screen.dart
@@ -1,8 +1,10 @@
 import 'dart:async';
 
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hahatonchikk/features/MainScreen/View/Main_Screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -64,6 +66,52 @@ class _LoginScreenState extends State<LoginScreen> {
         setState(() {
         });
       });
+  }
+
+  late String _originalCode;
+
+  Future _loginStart()async{
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String _smsCode = _password.text;
+    PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: _originalCode, smsCode: _smsCode);
+
+    // Sign the user in (or link) with the credential
+    await auth.signInWithCredential(credential);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const PageViewScreen(),
+      ),
+    );
+    /*if(auth.currentUser != null){
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const PageViewScreen(),
+        ),
+      );
+    } else {
+      print("object");
+    }*/
+  }
+
+  Future _getCode() async {
+    late String _code;
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    await auth.verifyPhoneNumber(
+      phoneNumber: _login.text,
+      verificationCompleted: (PhoneAuthCredential credential) {},
+      verificationFailed: (FirebaseAuthException e) {},
+      codeSent: (String verificationId, int? resendToken) async {
+        setState(() {
+          _originalCode = verificationId;
+        });
+        Fluttertoast.showToast(msg: "Ваш код был успешно выслан");
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+
   }
 
   @override
@@ -212,7 +260,7 @@ class _LoginScreenState extends State<LoginScreen> {
               flex: 1,
               child: InkWell(
                 onTap: () {
-
+                  _getCode();
                 },
                 borderRadius: BorderRadius.circular(15),
                 child: Ink(
@@ -252,6 +300,7 @@ class _LoginScreenState extends State<LoginScreen> {
               flex: 1,
               child: InkWell(
                 onTap: () {
+                  //_loginStart();
                   Navigator.push(
                     context,
                     MaterialPageRoute(
